a068d47ac5c224eae08a82f03a1f48bb85be7bb4
Iryna Chekh <chehirina@gmail.com>
2021-12-03 15:41:17 +0200

    create initial project

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+require('./bootstrap');
+
+require('alpinejs');
+
+import { createApp } from 'vue';
+import router from './router'
+
+import CompaniesIndex from './components/companies/CompaniesIndex.vue';
+
+createApp({
+    components: {
+        CompaniesIndex
+    }
+}).use(router).mount('#app')
+
+// import Alpine from 'alpinejs';
+//
+// window.Alpine = Alpine;
+//
+// Alpine.start();
