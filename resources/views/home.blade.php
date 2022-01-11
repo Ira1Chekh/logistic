bdf8cd3030dd269078a7a8d86189346d62efe5c7
Ira <chehirina@gmail.com>
2022-01-11 22:06:23 +0200

    add main page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <title>Logistics</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+        <a class="navbar-brand" href="#">Logist<span>ics</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Меню
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item"><a href="#services" class="nav-link">Послуги</a></li>
+                <li class="nav-item"><a href="#cargo_types" class="nav-link">Типи вантажу</a></li>
+                <li class="nav-item"><a href="#vehicle_types" class="nav-link">Типи кузова</a></li>
+                <li class="nav-item"><a href="#workflow" class="nav-link">Робочий процес</a></li>
+                <li class="nav-item"><a href="#about_us" class="nav-link">Про нас</a></li>
+                <li class="nav-item"><a href="#insurance" class="nav-link">Страхування</a></li>
+                <li class="nav-item"><a href="#contacts" class="nav-link">Контакти</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- END nav -->
+
+<div class="hero-wrap" style="background-image: url('/images/main.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text justify-content-start align-items-center">
+            <div class="col-lg-6 col-md-6 ftco-animate d-flex align-items-end">
+                <div class="text">
+                    <h1 class="mb-4">Доставка вантажів <span> по Україні </span> <span> від 30 кг</span></h1>
+                    <p style="font-size: 18px;">Створіть замовлення в один клік після реєстрації на сайті!</p>
+                </div>
+            </div>
+            <div class="col-lg-2 col"></div>
+            <div class="col-lg-4 col-md-6 mt-0 mt-md-5 d-flex">
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="ftco-section services-section ftco-no-pt ftco-no-pb" id="services">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 heading-section text-center ftco-animate mb-5">
+                <span class="subheading mt-4">Послуги</span>
+                <h2 class="mb-2">Послуги</h2>
+            </div>
+        </div>
+        <div class="row d-flex">
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services">
+                    <div class="media-body py-md-4">
+                        <div class="d-flex mb-3 align-items-center">
+                            <div class="icon"><span class="flaticon-customer-support"></span></div>
+                            <h3 class="heading mb-0 pl-3">Фінансова логістика</h3>
+                        </div>
+                        <ul>
+                            <li>Управляємо рухом фінансів при реалізації ЗЕД, що є важливою складовою логістичного процесу і може істотно впливати на підсумковий фінансовий результат</li>
+                            <li>Беремо участь на всіх етапах фінансового маршруту, роблячи його оптимальним і коректним</li>
+                            <li>Організовуємо оплати постачальнику</li>
+                            <li>Управляємо витратами і прибутком за зовнішньоекономічними операціями</li>
+                            <li>Фінансовий консалтинг</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services">
+                    <div class="media-body py-md-4">
+                        <div class="d-flex mb-3 align-items-center">
+                            <div class="icon"><span class="flaticon-online-booking"></span></div>
+                            <h3 class="heading mb-0 pl-3">Контрактна логістика</h3>
+                        </div>
+                        <ul>
+                            <li>Реалізуємо аутсорсинг відділу ЗЕД</li>
+                            <li>Візьмемо на себе перелік завдань по роботі з постачальниками</li>
+                            <li>Складемо і перевіримо зовнішньоекономічний контракт</li>
+                            <li>Узгодимо з постачальником спірні моменти по документам і процесам</li>
+                            <li>Поставки "під ключ"</li>
+                            <li>Послуга комісіонера</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services">
+                    <div class="media-body py-md-4">
+                        <div class="d-flex mb-3 align-items-center">
+                            <div class="icon"><span class="flaticon-route"></span></div>
+                            <h3 class="heading mb-0 pl-3">Транспортна логістика</h3>
+                        </div>
+                        <ul>
+                            <li>Організуємо доставку вантажу «від дверей до дверей» будь-яким видом транспорту, а також використовуючи комбінацію різних видів транспорту в ланцюжку поставки, з урахуванням дотримання основних логістичних критеріїв</li>
+                            <li>Розробимо оптимальний маршрут, запропонуємо оптимальні терміни і вартість при заданих умовах</li>
+                            <li>Доставимо Ваш вантаж в окремому авто або в складі збірного авто</li>
+                            <li>Контролюємо температурний режим при транспортуванні</li>
+                            <li>Організовуємо страхування вантажу</li>
+                            <li>Експедируємо вантажі в порту / аеропорту</li>
+                            <li>Відстежуємо вантажі на всіх етапах доставки</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="cargo_types">
+    <div class="container-fluid px-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12 heading-section text-center ftco-animate mb-5">
+                <span class="subheading">Типи вантажів</span>
+                <h2 class="mb-2">Які типи вантажів доставляємо?</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">01</a></h2>
+                        <span>Комерційний вантаж</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Mercedes Grand Sedan</a></h2>
+                        <span>Ford</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Mercedes Grand Sedan</a></h2>
+                        <span>Cheverolet</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="vehicle_types">
+    <div class="container-fluid px-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12 heading-section text-center ftco-animate mb-5">
+                <span class="subheading">Типи кузова</span>
+                <h2 class="mb-2">Які типи кузова є в автопарку?</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">01</a></h2>
+                        <span>Трал</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Mercedes Grand Sedan</a></h2>
+                        <span>Ford</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Mercedes Grand Sedan</a></h2>
+                        <span>Cheverolet</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section services-section img" id="workflow" style="background-image: url(/images/back.jpg);">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center heading-section heading-section-white ftco-animate">
+                <span class="subheading">Робочій процес</span>
+                <h2 class="mb-3">Як ми працюємо</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services services-2">
+                    <div class="media-body py-md-4 text-center">
+                        <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-route"></span></div>
+                        <h3>Оформлення замовлення</h3>
+                        <p>Заповнення форми з інформацією про тип вантажу, тип кузова, місце і дата відправлення і прибуття.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services services-2">
+                    <div class="media-body py-md-4 text-center">
+                        <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-select"></span></div>
+                        <h3>Перевірка документів менеджером</h3>
+                        <p>Менеджер переревіряє документи на перевезення, схвалює замовлення до оплати.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services services-2">
+                    <div class="media-body py-md-4 text-center">
+                        <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-rent"></span></div>
+                        <h3>Оплата</h3>
+                        <p>Клієнт оплачує перевезення банківскьим переводом.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services services-2">
+                    <div class="media-body py-md-4 text-center">
+                        <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-review"></span></div>
+                        <h3>Транспортування</h3>
+                        <p>Призначений до заказу водій завантажує вантаж у транспортний засіб.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex align-self-stretch ftco-animate">
+                <div class="media block-6 services services-2">
+                    <div class="media-body py-md-4 text-center">
+                        <div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-review"></span></div>
+                        <h3>Доставка до пункту призначення</h3>
+                        <p>Вантаж прибуває до місця призначення.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section ftco-no-pt ftco-no-pb" id="about_us">
+    <div class="container">
+        <div class="row no-gutters">
+            <div class="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(/images/about.jpg);">
+            </div>
+            <div class="col-md-6 wrap-about py-md-5 ftco-animate">
+                <div class="heading-section mb-5 pl-md-5">
+                    <span class="subheading">Про нас</span>
+                    <h2 class="mb-4">Чому варто вибрати Logistics?</h2>
+
+                    <p>«Logistics» – компанія, яка економить час своїх клієнтів. За 2 хвилини ми проконсультуємо, складемо заявку, розрахуємо вартість і підберемо автомобіль. Вантажна машина прибуває в вказане місце вже через 2 години, незалежно від ситуації на дорогах і дальності адреси.</p>
+                    <p>Величезна партнерська мережа, де налічується понад 3000 автомобілів, дозволяє нам не наймати посередників, працюючи з замовником безпосередньо. Це вберігає клієнта від низької якості послуг і дозволяє встановлювати ціни на 7-9% нижчими від ринкових.</p>
+                    <p>Оптимальні логістичні рішення, перевірені маршрути та чесні водії –  це те, що допомагає здійснювати перевезення завжди вчасно. Завдяки злагодженій роботі та комплексному підходу, наші автомобілі не затримуються, а ризик виникнення форс-мажорних ситуації зводиться до мінімуму.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section" id="insurance">
+    <div class="container-fluid px-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12 heading-section text-center ftco-animate mb-5">
+                <span class="subheading">Страхування</span>
+                <h2 class="mb-2">Ваш вантаж застрахований</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Відповідальність за вантаж</a></h2>
+                        <span>Ми за умовчанням страхуємо кожен вантаж для додаткової надійності</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Перевірені водії</a></h2>
+                        <span>100% машин пройшли перевірку та готові для роботи за європейськими стандартами</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="car-wrap ftco-animate">
+                    <div class="text p-4 text-center">
+                        <h2 class="mb-0"><a href="#">Будь-яка форма оплати</a></h2>
+                        <span>У нас можна платити готівкою, карткою або на розрахунковий рахунок</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="ftco-footer ftco-bg-dark ftco-section" id="contacts">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">Про Logistics</h2>
+                    <p>Logistics - логістична компанія з 2016 року на ринку.</p>
+                    <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                        <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                        <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="ftco-footer-widget mb-4">
+                    <h2 class="ftco-heading-2">У вас залишились запитання?</h2>
+                    <div class="block-23 mb-3">
+                        <ul>
+                            <li><span class="icon icon-map-marker"></span><span class="text">вул. Сумська 27, Харків, Україна</span></li>
+                            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+38(050) 052 21 46</span></a></li>
+                            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">logisticts@gmail.com</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+</body>
+</html>
